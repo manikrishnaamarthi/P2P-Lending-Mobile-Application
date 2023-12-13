7c114ae9e49d4ee75362f3a21509dce583bb3d0e
Yarisingu तरुण <137394306+yarasingu@users.noreply.github.com>
2023-12-13 11:46:13 +0000

    main.py login_screen signup_screen serveces

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
-
 from screens.home_screen import HomeScreen
+from screens.login_screen import LoginScreen
+from screens.signup_screen import SignupScreen
 
 
 class P2PLendingApp(App):
+
     def build(self):
-        # Create a ScreenManager
-        screen_manager = ScreenManager()
+        self.screen_manager = ScreenManager()
 
-        # Add the HomeScreen to the ScreenManager
-        home_screen = HomeScreen(name='home')
-        screen_manager.add_widget(home_screen)
+        # Check if user is logged in
+        is_logged_in = self.check_logged_in()
 
-        return screen_manager
+        # Add appropriate screen based on login status
+        if is_logged_in:
+            home_screen = HomeScreen(name='home')
+            self.screen_manager.add_widget(home_screen)
+        else:
+            login_screen = LoginScreen(name='login')
+            self.screen_manager.add_widget(login_screen)
+            signup_screen = SignupScreen(name='signup')
+            self.screen_manager.add_widget(signup_screen)
+
+        return self.screen_manager
 
 
 if __name__ == '__main__':
